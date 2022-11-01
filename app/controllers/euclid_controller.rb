2a7aa4598ca7f9ff9b36734ca09c5d0f46f4dcf0
reddiridabl666 <reddiridabl666@gmail.com>
2022-11-01 13:24:58 +0300

    lab8 done

--- app/controllers/euclid_controller.rb
+++ app/controllers/euclid_controller.rb
@@ -5,5 +5,8 @@ class EuclidController < ApplicationController
   def result
     @first_num = params[:input_1].to_i
     @second_num = params[:input_2].to_i
+
+    @first_res = @first_num
+    @second_res = @second_num
   end
 end
