5616b3242d9ad366cf83f527ff6e00a554ec4337
reddiridabl666 <reddiridabl666@gmail.com>
2022-11-06 20:20:45 +0300

    A

--- app/controllers/euclid_controller.rb
+++ app/controllers/euclid_controller.rb
@@ -1,16 +1,48 @@
 class EuclidController < ApplicationController
-  def index
-  end
-
+  protect_from_forgery except: :index
   def valid(input)
     return false if input.to_i == 0
     /\A\d+\Z/.match?(input)
   end
 
-  def result
-    unless valid(params[:input_1]) && valid(params[:input_2])
-      return redirect_to "/", alert: "You should enter a natural number in each field"
+  def index
+    # if request.xhr?
+    unless params[:input_1].nil? && params[:input_2].nil?
+      unless valid(params[:input_1]) && valid(params[:input_2])
+        flash.alert = "You should enter a natural number in each field"
+      end
+
+      @first_num = params[:input_1].to_i
+      @second_num = params[:input_2].to_i
+
+      first_res = @first_num
+      second_res = @second_num
+
+      iter = 0
+      @results = Enumerator.new do |yielder|
+        loop do
+          if first_res < second_res
+            yielder << [iter += 1, first_res, second_res -= first_res]
+          else
+            yielder << [iter += 1, first_res -= second_res, second_res]
+          end
+        end
+      end.take_while { |_, first, second | [first, second].min != 0 }
+
+      @gcd = [first_res, second_res].max
+      @lcm = @first_num * @second_num / @gcd
+
+      respond_to do |format|
+        format.js
+        format.html
+      end
     end
+  end
+
+  def result
+    # unless valid(params[:input_1]) && valid(params[:input_2])
+    #   return redirect_to "/", alert: "You should enter a natural number in each field"
+    # end
 
     @first_num = params[:input_1].to_i
     @second_num = params[:input_2].to_i
@@ -32,9 +64,11 @@ class EuclidController < ApplicationController
     @gcd = [first_res, second_res].max
     @lcm = @first_num * @second_num / @gcd
 
+    raise "Fuck it" unless request.xhr?
+
     respond_to do |format|
       format.js
-      # format.html
+      format.html
     end
   end
 end
