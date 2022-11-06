a72da41686913c07efd4e521319254f99b987dc0
reddiridabl666 <reddiridabl666@gmail.com>
2022-11-06 15:46:32 +0300

    Added tests for controllers in lab8

--- app/controllers/euclid_controller.rb
+++ app/controllers/euclid_controller.rb
@@ -3,30 +3,33 @@ class EuclidController < ApplicationController
   end
 
   def valid(input)
-    return false if input.blank?
+    return false if input.to_i == 0
     /\A\d+\Z/.match?(input)
   end
 
   def result
     unless valid(params[:input_1]) && valid(params[:input_2])
-      redirect_to "/", alert: "You should enter a natural number in each field"
+      return redirect_to "/", alert: "You should enter a natural number in each field"
     end
 
     @first_num = params[:input_1].to_i
     @second_num = params[:input_2].to_i
 
-    @first_res = @first_num
-    @second_res = @second_num
+    first_res = @first_num
+    second_res = @second_num
 
     iter = 0
     @results = Enumerator.new do |yielder|
       loop do
-        if @first_res < @second_res
-          yielder << [iter += 1, @first_res, @second_res -= @first_res]
+        if first_res < second_res
+          yielder << [iter += 1, first_res, second_res -= first_res]
         else
-          yielder << [iter += 1, @first_res -= @second_res, @second_res]
+          yielder << [iter += 1, first_res -= second_res, second_res]
         end
       end
     end.take_while { |_, first, second | [first, second].min != 0 }
+
+    @gcd = [first_res, second_res].max
+    @lcm = @first_num * @second_num / @gcd
   end
 end
