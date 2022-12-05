b041323a1aecbf65c2e11ef317e45d3e81997c5a
reddiridabl666 <reddiridabl666@gmail.com>
2022-12-05 12:55:02 +0300

    Added test for errors

--- spec/system/euclid_system_spec.rb
+++ spec/system/euclid_system_spec.rb
@@ -7,7 +7,18 @@ RSpec.describe "user input", type: :system do
     fill_in "input_2", with: 36
     click_button "Calculate greatest common denominator"
 
+    expect(page).to have_current_path root_path
     expect(find('#gcd')).to have_content '12'
     expect(find('#lcm')).to have_content '72'
+  end
+
+  it "returns valid answers" do
+    visit root_path
+    fill_in "input_1", with: -5
+    fill_in "input_2", with: 36
+    click_button "Calculate greatest common denominator"
+
+    expect(page).to have_current_path root_path
+    expect(page).to have_content "You should enter a natural number in each field"
   end
 end
