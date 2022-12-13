a9d849103c2a0e76177a6a5c534e2daa8d2b786a
reddiridabl666 <reddiridabl666@gmail.com>
2022-12-13 11:00:08 +0300

    refactored lab9

--- app/controllers/euclid_controller.rb
+++ app/controllers/euclid_controller.rb
@@ -7,8 +7,8 @@ class EuclidController < ApplicationController
   def index; end
 
   def result
-    @first_num = params[:input_1].to_i
-    @second_num = params[:input_2].to_i
+    @first_num = params[:input1].to_i
+    @second_num = params[:input2].to_i
 
     @results, @gcd, @lcm = euclid_algorithm
   end
@@ -17,8 +17,8 @@ class EuclidController < ApplicationController
 
   def euclid_algorithm
     results = euclid_enumerator(@first_num, @second_num)
-                .each_with_index
-                .take_while {|res, _| [res[0], res[1]].min != 0 }
+              .each_with_index
+              .take_while { |res, _| [res[0], res[1]].min != 0 }
 
     gcd = results.blank? ? @first_num : results[-1][0].max
     lcm = @first_num * @second_num / gcd
@@ -39,13 +39,14 @@ class EuclidController < ApplicationController
   end
 
   def validate_input
-    unless valid(params[:input_1]) && valid(params[:input_2])
-      redirect_to root_path, alert: "You should enter a natural number in each field"
+    unless valid(params[:input1]) && valid(params[:input2])
+      redirect_to root_path, alert: 'You should enter a natural number in each field'
     end
   end
 
   def valid(input)
-    return false if input.to_i == 0
-    /\A\d+\Z/.match?(input)
+    return false if input.to_i.zero?
+
+    /\A\d+\z/.match?(input)
   end
 end
--- spec/system/euclid_system_spec.rb
+++ spec/system/euclid_system_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe "user input", type: :system do
   it "returns valid answers" do
     visit root_path
-    fill_in "input_1", with: 24
-    fill_in "input_2", with: 36
+    fill_in "input1", with: 24
+    fill_in "input2", with: 36
     click_button "Calculate greatest common denominator"
 
     expect(page).to have_current_path root_path
@@ -12,10 +12,20 @@ RSpec.describe "user input", type: :system do
     expect(find('#lcm')).to have_content '72'
   end
 
-  it "returns valid answers" do
+  it "shows error on negative numbers" do
     visit root_path
-    fill_in "input_1", with: -5
-    fill_in "input_2", with: 36
+    fill_in "input1", with: -5
+    fill_in "input2", with: 36
+    click_button "Calculate greatest common denominator"
+
+    expect(page).to have_current_path root_path
+    expect(page).to have_content "You should enter a natural number in each field"
+    end
+
+  it "shows error on invalid input" do
+    visit root_path
+    fill_in "input1", with: 12
+    fill_in "input2", with: 'fds'
     click_button "Calculate greatest common denominator"
 
     expect(page).to have_current_path root_path
